ef1dd182fde72e0ed6896e8cb429c6ead2952d3c
Bilal Akhtar <me@itsbilal.com>
2014-09-01 22:07:59 -0400

    Initial Commit

---
+++ gardiner-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gardiner-ios
+//
+//  Created by Bilal Akhtar on 2014-09-01.
+//  Copyright (c) 2014 Bilal Akhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
