4485ff93d8dcb9ecc946859d82b87dbf3074a8b2
Bilal Akhtar <me@itsbilal.com>
2014-12-13 14:47:37 -0500

    Some progress on add place views

---
+++ gardiner-ios/NewPlaceViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NewPlaceViewController.swift
+//  gardiner-ios
+//
+//  Created by Bilal Akhtar on 2014-12-12.
+//  Copyright (c) 2014 Bilal Akhtar. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onCancelButtonClick(sender: AnyObject) {
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
